daa925a115ede10c0c94eb6f461aca2db1cad19a
user <user@192.168.1.106>
2024-01-05 14:06:57 +0900

    init

---
+++ src/generics_sample/Main01.java
@@ -0,0 +1,12 @@
+package generics_sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main01 {
+	public static void main(String[] args) {
+		// ArrayList(変数名：list）をString型で宣言・生成
+
+		List<String> list = new ArrayList<>();
+	}
+}
